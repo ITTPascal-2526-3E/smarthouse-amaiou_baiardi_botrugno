20e40e2998269270124fb2309812dd4fd43aeeef
BaiardiVittorio <vittoriobaiardi09@gmail.com>
2025-10-28 13:59:24 +0100

    Lamp model

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -0,0 +1,14 @@
+﻿namespace BlaisePascal.SmartHouse.Domain
+{
+    public class Lamp
+    {
+        private bool isOn;
+        public int consumptions { get; set; }
+        public string lampType { get; set; }
+        public double lampHeat { get; set; }
+        public string energyClass { get; set; }
+        public int brightness { get; set; }
+        public int lumen { get; set; }
+
+    }
+}
