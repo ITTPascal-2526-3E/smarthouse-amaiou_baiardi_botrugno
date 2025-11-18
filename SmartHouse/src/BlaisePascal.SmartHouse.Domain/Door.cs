cb0ce262506791d74e503c98e392c40aec4e5e42
BotrugnoMattia <mattia.botrugno02@gmail.com>
2025-11-18 13:54:14 +0100

    inizio classe Door 18/11/2025

--- SmartHouse/src/BlaisePascal.SmartHouse.Domain/Door.cs
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Door.cs
@@ -8,6 +8,32 @@ namespace BlaisePascal.SmartHouse.Domain
 {
     public class Door
     {
+        private bool isOpen = false;
+        private string material {  get; set; }
+        private string type {  get; set; }
+        private bool isBulletProof = true;
+        private bool isEnterHouseDoor = true;
+        private bool isInsideHouseDoor = false;
+        private double height, length, width;
 
+        public void changeDoorState() 
+        {
+            isOpen = true;
+        }
+
+        public void changeLength(double value) 
+        { 
+            length = value;
+        }
+
+        public void changeWidth(double value)
+        {
+            width = value;
+        }
+
+        public void changeHeight(double value)
+        {
+            height = value;
+        }
     }
 }
