6fc2a98ef3f535a46886948a44ce6e84efd818e1
akettina <haron.amaiou.stud@ispascalcomandini.it>
2025-12-16 13:19:09 +0100

    class device

--- SmartHouse/src/BlaisePascal.SmartHouse.Domain/Devices/Device.cs
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Devices/Device.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace BlaisePascal.SmartHouse.Domain.Devices
 {
-    public class Device
+    public class Device 
     {
-        
+        public Guid id { get; protected set; }
+        public string name { get; protected set; }
+        public bool status { get; protected set; }
+        public string timeOfCreation { get; protected set; }
+        public string lastTimeModified { get; protected set; }
+        public Device(string Name, bool Status, string TimeOfCreation,string LastTimeModified) 
+        { 
+            id = Guid.NewGuid();
+            name = Name;
+            status = Status;
+            timeOfCreation = TimeOfCreation;
+            lastTimeModified = LastTimeModified;
+        }
     }
 }
--- SmartHouse/src/BlaisePascal.SmartHouse.Domain/Devices/Temp_devices/Thermostat.cs
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Devices/Temp_devices/Thermostat.cs
@@ -9,7 +9,7 @@ namespace BlaisePascal.SmartHouse.Domain.Devices.Temp_devices
     public class Thermostat 
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        private AirConditioner airConditioner = new AirConditioner();
+        private AirConditioner airConditioner = new AirConditioner(18.00);
         public void turnOnAirConditioner() 
         {
             airConditioner.turnOn();
@@ -37,9 +37,9 @@ namespace BlaisePascal.SmartHouse.Domain.Devices.Temp_devices
             airConditioner.decreaseTemp();
 
         }
-        public void setName()
+        public void setName(string AirConditionerName)
         {
-           
+           airConditioner.setName(AirConditionerName);
         }
 
     }
