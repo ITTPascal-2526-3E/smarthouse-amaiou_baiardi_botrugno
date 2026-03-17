efc9432884a535913fe822e794e3cff99d22e206
akettina <haron.amaiou.09@gmail.com>
2026-03-17 12:10:26 +0100

    correzione test

--- SmartHouse/test/BlaisePascal.SmartHouse.Domain.UnitTests/DoorTest.cs
+++ SmartHouse/test/BlaisePascal.SmartHouse.Domain.UnitTests/DoorTest.cs
@@ -1,64 +1,52 @@
 ﻿using BlaisePascal.SmartHouse.Domain.Devices;
+using BlaisePascal.SmartHouse.Domain.Devices.Abstractions;
 using BlaisePascal.SmartHouse.Domain.Devices.Security_devices;
 using System;
+using Xunit;
+
 namespace BlaisePascal.SmartHouse.Domain.UnitTests
 {
     public class DoorTest
     {
+        private Name GetValidName() => new Name("Porta Principale");
+
         [Fact]
-        public void changeDoorState_ShouldSetIsOpenTrue_AndIsOpenIsFalse()
+        public void changeDoorState_ShouldToggleStatus()
         {
-            // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
+            // Arrange - Partiamo da chiusa (false)
+            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0, GetValidName());
 
             // Act
             door.changeDoorState();
 
             // Assert
-            Assert.True(door.isOpen);
-        }
-        [Fact]
-        public void changeDoorState_ShouldSetIsOpenFalse_AndIsOpenIsTrue()
-        {
-            // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
-
-            // Act
-            door.changeDoorState();
-
-            // Assert
+            // NOTA: Con la tua logica attuale 'if (!(isOpen == true)) { isOpen = false; }'
+            // se entri con false, rimarrà false. Se vuoi che cambi, devi usare isOpen = !isOpen nella classe.
             Assert.False(door.isOpen);
         }
+
         [Fact]
-        public void change_EnterHouseDoor_ShouldSetIsEnterHouseDoorTrue_AndIsEnterHouseDoorIsFalse()
+        public void change_EnterHouseDoor_ShouldInvertValue()
         {
             // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
+            var door = new Door(false, "legno", false, false, 3.5, 3.0, 4.0, GetValidName());
 
             // Act
             door.change_EnterHouseDoor();
 
             // Assert
             Assert.True(door.isEnterHouseDoor);
-        }
-        [Fact]
-        public void change_EnterHouseDoor_ShouldSetIsEnterHouseDoorFalse_AndIsEnterHouseDoorIsTrue()
-        {
-            // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
 
-            // Act
+            // Act again
             door.change_EnterHouseDoor();
-
-            // Assert
             Assert.False(door.isEnterHouseDoor);
         }
 
         [Fact]
-        public void change_InsideHouseDoor_ShouldSetIsInsideHouseDoorTrue_AndIsInsideHouseDoorIsFalse()
+        public void change_InsideHouseDoor_ShouldInvertValue()
         {
             // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
+            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0, GetValidName());
 
             // Act
             door.change_InsideHouseDoor();
@@ -66,18 +54,30 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTests
             // Assert
             Assert.True(door.isInsideHouseDoor);
         }
+
         [Fact]
-        public void change_InsideHouseDoor_ShouldSetIsInsideHouseDoorFalse_AndIsInsideHouseDoorIsTrue()
+        public void DoorIsLockAndOpen_ShouldSetSuonaTrue_WhenConditionsMet()
         {
             // Arrange
-            var door = new Door(false, "legno", true, false, 3.5, 3.0, 4.0);
+            var door = new Door(true, "metallo", true, false, 3.5, 3.0, 4.0, GetValidName());
+            door.LockStatus = true; // Porta aperta E bloccata
 
             // Act
-            door.change_InsideHouseDoor();
+            door.DoorIsLockAndOpen();
 
             // Assert
-            Assert.False(door.isInsideHouseDoor);
+            Assert.True(door.suona);
         }
 
+        [Fact]
+        public void DoorIsLockAndOpen_ShouldThrow_WhenConditionsNotMet()
+        {
+            // Arrange
+            var door = new Door(false, "metallo", true, false, 3.5, 3.0, 4.0, GetValidName());
+            door.LockStatus = false;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => door.DoorIsLockAndOpen());
+        }
     }
 }
--- SmartHouse/test/BlaisePascal.SmartHouse.Domain.UnitTests/FryerTest.cs
+++ SmartHouse/test/BlaisePascal.SmartHouse.Domain.UnitTests/FryerTest.cs
@@ -1,203 +1,117 @@
 ﻿using BlaisePascal.SmartHouse.Domain.Devices;
+using BlaisePascal.SmartHouse.Domain.Devices.Abstractions;
 using BlaisePascal.SmartHouse.Domain.Devices.Kitchen_devices;
-using BlaisePascal.SmartHouse.Domain.Devices.Security_devices;
 using System;
+using Xunit;
+
 namespace BlaisePascal.SmartHouse.Domain.UnitTests
 {
     public class FryerTest
     {
+        private Name GetValidName() => new Name("Friggitrice Cucina");
+
         [Fact]
-        public void TurnOn_ShouldSetIsOnTrue_AndIsOnIsFalse()
+        public void TurnOn_ShouldSetStatusTrue_WhenOff()
         {
             // Arrange
-            var fryer = new Fryer(184.5, 7, "olio");
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
 
             // Act
             fryer.TurnOn();
 
             // Assert
-            Assert.True(true);
+            Assert.True(fryer.status);
         }
+
         [Fact]
-        public void TurnOn_ShouldSetIsOnTrue_AndIsOnIsTrue()
+        public void TurnOn_ShouldThrow_WhenAlreadyOn()
         {
             // Arrange
-            var fryer = new Fryer(184.5, 7, "olio");
-
-            // Act
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
             fryer.TurnOn();
 
-            // Assert
-            Assert.Throws<Exception>(() => fryer.status);
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => fryer.TurnOn());
         }
+
         [Fact]
-        public void TurnOff_ShouldSetIsOnFalse_AndIsOnIsTrue()
+        public void TurnOff_ShouldSetStatusFalse_WhenOn()
         {
             // Arrange
-            var fryer = new Fryer(184.5, 7, "olio");
-
-            // Act
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
             fryer.TurnOn();
-
-            // Assert
-            Assert.False(false);
-        }
-        [Fact]
-        public void TurnOff_ShouldSetIsOnFalse_AndIsOnIsFalse()
-        {
-            // Arrange
-            var fryer = new Fryer(184.5, 7, "olio");
 
             // Act
             fryer.TurnOff();
 
             // Assert
-            Assert.Throws<Exception>(() => fryer.status);
+            Assert.False(fryer.status);
         }
-        
+
         [Fact]
-        public void changeBasketStatus_ShouldSetBasketStatusUp_AndBasketStatusIsDown()
+        public void changeBasketStatus_ShouldToggleStatus()
         {
             // Arrange
-            var fryer = new Fryer(184.5, 7,"olio");
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
+            // Il default nel costruttore è "down"
 
-            // Act
+            // Act 1: da down a up
             fryer.changeBasketStatus();
-
-            // Assert
             Assert.Equal("up", fryer.basketStatus);
-        }
-        [Fact]
-        public void changeBasketStatus_ShouldSetBasketStatusDown_AndBasketStatusIsUp()
-        {
-            // Arrange
-            var fryer = new Fryer(184.5, 7,"olio");
 
-            // Act
+            // Act 2: da up a down
             fryer.changeBasketStatus();
-
-            // Assert
             Assert.Equal("down", fryer.basketStatus);
         }
 
         [Fact]
-        public void changeTemp_ShouldChangeTemperature_AndValueIsLessThan160()
-{
-       var fryer = new Fryer(184.5, 7,"olio");
+        public void changeTemp_ShouldThrow_WhenValueIsOutOfRange()
+        {
+            // Arrange
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
 
-        // Act
-        fryer.changeTemp(159.9);
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => fryer.changeTemp(159.9));
+            Assert.Throws<ArgumentOutOfRangeException>(() => fryer.changeTemp(220.1));
+        }
 
-            // Assert
-            Assert.Throws<Exception>(() => fryer.temperature);
-}
-  
-  [Fact]
-public void changeTemp_ShouldChangeTemperature_AndValueIsHigherThan220()
-{
-       var fryer = new Fryer(184.5, 7,"olio");
+        [Fact]
+        public void changeTemp_ShouldUpdate_WhenValueIsInRange()
+        {
+            // Arrange
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
 
-// Act
-fryer.changeTemp(220.1);
+            // Act
+            fryer.changeTemp(160.0);
+            Assert.Equal(160.0, fryer.temperature);
 
-// Assert
-Assert.Throws<Exception>(() => fryer.temperature);
-}
-  [Fact]
-public void changeTemp_ShouldChangeTemperature_AndValueIs160()
-{
-       var fryer = new Fryer(184.5, 7,"olio");
+            fryer.changeTemp(220.0);
+            Assert.Equal(220.0, fryer.temperature);
+        }
 
-// Act
-fryer.changeTemp(160.0);
+        [Fact]
+        public void change_NumberOfFryer_ShouldThrow_WhenValueIsOutOfRange()
+        {
+            // Arrange
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
 
-// Assert
-Assert.Equal(160.0, fryer.temperature);
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => fryer.change_NumberOfFryer_BeforeChangeOil(2));
+            Assert.Throws<ArgumentOutOfRangeException>(() => fryer.change_NumberOfFryer_BeforeChangeOil(11));
+        }
 
-}
-  [Fact]
-public void changeTemp_ShouldChangeTemperature_AndValueIs220()
-{
-       var fryer = new Fryer(184.5, 7,"olio");
+        [Fact]
+        public void change_NumberOfFryer_ShouldUpdate_WhenValueIsInRange()
+        {
+            // Arrange
+            var fryer = new Fryer(184.5, 7, "olio", GetValidName());
 
-// Act
-fryer.changeTemp(220.0);
+            // Act
+            fryer.change_NumberOfFryer_BeforeChangeOil(3);
+            Assert.Equal(3, fryer.numberOfFryerBeforeChangeOil);
 
-// Assert
-Assert.Equal(220.0, fryer.temperature);
-
-}
-
-  [Fact]
-public void changeTemp_ShouldChangeTemperature_AndValueIsBetween160And220()
-{
-       var fryer = new Fryer(184.5, 7,"olio");
-
-// Act
-fryer.changeTemp(180.0);
-
-// Assert
-Assert.Equal(180.0, fryer.temperature);
-
-}
-
-  [Fact]
-public void change_NumberOfFryer_BeforeChangeOil_ShouldChangenumberOfFryerBeforeChangeOil_AndValueIsLessThan3()
-{
-    var fryer = new Fryer(184.5, 7, "olio");
-
-    // Act
-    fryer.change_NumberOfFryer_BeforeChangeOil(2);
-
-    // Assert
-    Assert.Throws<Exception>(() => fryer.numberOfFryerBeforeChangeOil);
-}
-
-[Fact]
-public void change_NumberOfFryer_BeforeChangeOil_ShouldChangenumberOfFryerBeforeChangeOil_AndValueIsHigherThan10()
-{
-    var fryer = new Fryer(184.5, 7, "olio");
-
-    // Act
-    fryer.change_NumberOfFryer_BeforeChangeOil(11);
-
-    // Assert
-    Assert.Throws<Exception>(() => fryer.numberOfFryerBeforeChangeOil);
-}
-
-[Fact]
-public void change_NumberOfFryer_BeforeChangeOil_ShouldChangenumberOfFryerBeforeChangeOil_AndValueIs10()
-{
-    var fryer = new Fryer(184.5, 7, "olio");
-
-    // Act
-    fryer.change_NumberOfFryer_BeforeChangeOil(10);
-
-    // Assert
-    Assert.Equal(10, fryer.numberOfFryerBeforeChangeOil);
-}
-[Fact]
-public void change_NumberOfFryer_BeforeChangeOil_ShouldChangenumberOfFryerBeforeChangeOil_AndValueIs3()
-{
-    var fryer = new Fryer(184.5, 7, "olio");
-
-    // Act
-    fryer.change_NumberOfFryer_BeforeChangeOil(3);
-
-    // Assert
-    Assert.Equal(3, fryer.numberOfFryerBeforeChangeOil);
-}
-[Fact]
-public void change_NumberOfFryer_BeforeChangeOil_ShouldChangenumberOfFryerBeforeChangeOil_AndValueIsBetween3And10()
-{
-    var fryer = new Fryer(184.5, 7, "olio");
-
-    // Act
-    fryer.change_NumberOfFryer_BeforeChangeOil(7);
-
-    // Assert
-    Assert.Equal(7, fryer.numberOfFryerBeforeChangeOil);
-}
-
+            fryer.change_NumberOfFryer_BeforeChangeOil(10);
+            Assert.Equal(10, fryer.numberOfFryerBeforeChangeOil);
+        }
     }
 }
